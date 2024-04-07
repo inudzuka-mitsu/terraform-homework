af061f898766799fbeb751d40114b5142a98c0a4
inudzuka-mitsu <valeriiababina.qa@gmail.com>
2024-04-07 16:43:08 +0000

    homework 3

---
+++ Homework3/main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "aws_ami" "aws-linux-2" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
+}
+
+data "aws_availability_zones" "azs" {}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.aws-linux-2.id
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ aws_security_group.allow_tls.id ]
+  key_name = aws_key_pair.bast-key.key_name
+  associate_public_ip_address = true
+  user_data = file("apache.sh")
+  user_data_replace_on_change = true
+  availability_zone = data.aws_availability_zones.azs.names[count.index]
+  count         = 3
+
+  tags = {
+    Name = "ec2-${count.index}"
+  }
+}
